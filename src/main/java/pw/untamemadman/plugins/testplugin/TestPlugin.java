2721943bd3b99630b7241f49c12f47bdde49febd
untamemadman <sam@untamemadman.pw>
2015-03-01 13:18:20 +0000

    Added a command

--- src/main/java/pw/untamemadman/plugins/testplugin/TestPlugin.java
+++ src/main/java/pw/untamemadman/plugins/testplugin/TestPlugin.java
@@ -1,7 +1,10 @@
 package pw.untamemadman.plugins.testplugin;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import pw.untamemadman.plugins.testplugin.listeners.onCommand;
 import pw.untamemadman.plugins.testplugin.listeners.onLogin;
 
 /**
@@ -14,8 +17,36 @@ public class TestPlugin extends JavaPlugin
     public void onEnable()
     {
         getLogger().info("onEnable has been invoked!"); //Prints that "onEnable" has been called
-        getServer().getPluginManager().registerEvents(new onLogin(), this);
-        getServer().getPluginManager().registerEvents(new onCommand(), this);
+        //getServer().getPluginManager().registerEvents(new onLogin(), this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+        if (cmd.getName().equalsIgnoreCase("untamemadman"))
+        // If the player typed /untamemadman then do the following...
+        {
+            if (sender instanceof Player)
+            {
+                Player player = (Player) sender;
+                sender.sendMessage("Thanks for using my plugin!");
+                sender.sendMessage("This is version " + ChatColor.GREEN + "1.0");
+                sender.sendMessage("for more information go to my website");
+                sender.sendMessage(ChatColor.BLUE  + "http://plugins.untamemadman.pw");
+                sender.sendMessage(ChatColor.BLUE  + "http://modding.untamemadman.pw");
+                sender.sendMessage("View my blog here");
+                sender.sendMessage(ChatColor.BLUE  + "http://blog.untamemadman.pw");
+                sender.sendMessage("How about supporting me?");
+                sender.sendMessage(ChatColor.BLUE  + "http://patreon.untamemadman.pw");
+                return true;
+            }
+            else
+            {
+                sender.sendMessage("You must be a player!");
+                return false;
+            }
+        }
+        return false;
     }
 
     @Override
